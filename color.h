d1bb3c8c8c99f621a7d1041c4d720e3b5e1f5faf
Saka1r <sakairlinux@gmail.com>
2025-12-03 13:19:43 +0300

    For windows

--- color.h
+++ color.h
@@ -18,4 +18,21 @@ void color_printf(const char text[], const char color[]);
 
 #endif
 
+#ifdef _WIN32
+
+#define BLACK "\033[0;30m"
+#define RED "\033[0;31m"
+#define GREEN "\033[0;32m"
+#define YELLOW "\033[0;33m"
+#define BLUE "\033[0;34m"
+#define MAGENTA "\033[0;35m"
+#define CYAN "\033[0;36m"
+#define WHITE "\033[0;37m"
+
+#define RESET "\033[0m"
+
+void color_printf(const char text[], const char color[]);
+
+#endif
+
 #endif
